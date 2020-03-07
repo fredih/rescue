fd5dd995b9cde782bf403e2ac389af7b9e546231
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2020-03-07 11:56:18 -0300

    added bugged raycasting to Lobin

--- characters/lobins/lobin.gd
+++ characters/lobins/lobin.gd
@@ -3,15 +3,21 @@ extends Actor
 var target
 var playerPointer
 var velocity = Vector2()
+var result
 
 func _ready():
 	pass # Replace with function body.
 
 
 func _process(delta):
-	if target.isLit:
+	velocity = target.position - position
+	velocity.y = 0
+	velocity = velocity.normalized()
+	
+	if target.isLit and result.collider == target:
+		position += velocity * delta * speed.x
 		
 		
 func _physics_process(delta):
 	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(global_position, enemy_position, [self])
+	result = space_state.intersect_ray(position, target.position, [self])
