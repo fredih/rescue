aa44820d785929cb89500050f62b40b5fa744665
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2020-03-07 17:10:25 -0300

    add animation for lobin and edited lobin's sript

--- characters/lobins/lobin.gd
+++ characters/lobins/lobin.gd
@@ -3,19 +3,38 @@ extends Actor
 var target
 var playerPointer
 var result
-
+var state = "scared"
+export var maxDistance = 10
 func _ready():
-	pass # Replace with function body.
-
+	speed.x = 1.5
+	gravity = 800
 
 func _process(delta):
-	_velocity.x = target.position.x - position.x
-	_velocity = _velocity.normalized()
+	pass
+	#_velocity = _velocity.normalized()
 	
-	if target.isLit and result.collider == target:
-		position.x += _velocity.x * delta * speed.x
 		
-func _physics_process(delta):
-	var space_state = get_world_2d().direct_space_state
-	result = space_state.intersect_ray(position, target.position, [self])
-	._physics_process(delta)
+func _physics_process(delta: float) -> void:
+	match state:
+		"happy":
+			$lobinSprite.animation = "Idle"
+			if abs(target.position.x - position.x) > maxDistance:
+				state = "scared"
+				$lobinSprite.animation = "Walk"
+			move_and_slide(_velocity, FLOOR_NORMAL, false)
+			
+		"scared":
+			#raycasting player
+			var space_state = get_world_2d().direct_space_state
+			result = space_state.intersect_ray(position, target.position, [self])
+			#if ray hits player, move. If not, don't
+			if result and target.isLit and result.collider == target:
+				_velocity.x = speed.x * (target.position.x - position.x)
+			else:
+				_velocity.x = 0
+			move_and_slide(_velocity, FLOOR_NORMAL, false)
+			if abs(target.position.x - position.x) < maxDistance:
+				_velocity.x = 0
+				state = "happy"
+		
+		
