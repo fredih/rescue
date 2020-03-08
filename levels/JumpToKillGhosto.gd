ffec133ff0f71b65a8b2fbf8f543a20b972ce108
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-08 18:19:47 -0300

    adding level with multiple jumpings to kill ghosto

---
+++ levels/JumpToKillGhosto.gd
@@ -0,0 +1,16 @@
+extends Baselevel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$DownGhosto/ScreamArea/Collider.position.y = 60
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
