a343ec0771538269a16c6be652ff3594d2176bc3
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-08 14:29:27 -0300

    change first scene to splash screen

--- splash/SplashScreen.gd
+++ splash/SplashScreen.gd
@@ -13,16 +13,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_start"):
+		get_tree().change_scene("res://levels/NoEnemies.tscn")
 	if $Camera2D.position.y < 330:
 		$Camera2D.position.y += 1.6
 	
 	else:
 		if $RescueTitleText.position.y < 385:
-			if $RescueTitleText.position.y < 90:
-				print($RescueTitleText.position.y)
+			if $RescueTitleText.position.y < 100:
 				$RescueTitleText.position.y += 30
 			else:
-				$RescueTitleText.position.y += 2.8
+				$RescueTitleText.position.y += 3.2
 		else:
 			if $pressStart.position.y > 450:
 				$pressStart.position.y -= 1.2
